aeb472b86d7fc6b00819d841d643a88796d1a9fe
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2026-06-28 22:12:15 +0530

    🟢 Add 0009. Palindrome Number [Easy] (C++)

---
+++ Easy/0009-Palindrome-Number/solution.cpp
@@ -0,0 +1,30 @@
+// ═══════════════════════════════════════════════════════
+//  Problem  : 0009. Palindrome Number
+//  URL      : https://leetcode.com/problems/palindrome-number/submissions/2049151068/
+//  Difficulty : Easy
+//  Language : C++
+//  Runtime  : 0 ms
+//  Memory   : 8.5 MB
+//  Solved   : June 28, 2026
+// ═══════════════════════════════════════════════════════
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        // negative ya last digit 0 (except 0)
+        if(x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int rev = 0;
+
+        while(x > rev){
+            int last = x % 10;
+            rev = rev * 10 + last;
+            x = x / 10;
+        }
+
+        // even digits → x == rev
+        // odd digits → x == rev/10
+        return (x == rev || x == rev/10);
+    }
+};
