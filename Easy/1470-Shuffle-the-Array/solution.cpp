9625c3af1285ab0658f98b35a4a0df99d57a8595
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2026-09-08 00:18:56 +0530

    🟢 Add 1470. Shuffle the Array [Easy] (C++)

--- Easy/1470-Shuffle-the-Array/solution.cpp
+++ Easy/1470-Shuffle-the-Array/solution.cpp
@@ -4,14 +4,19 @@
 //  Difficulty : Easy
 //  Language : C++
 //  Runtime  : 0 ms
-//  Memory   : 8.3 MB
-//  Solved   : August 19, 2026
+//  Memory   : 8.4 MB
+//  Solved   : September 8, 2026
 // ═══════════════════════════════════════════════════════
 
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
+        vector <int> ans(2 * n);
+        for(int i = 0 ; i < n ; i++ ){
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[i + 1];
 
-     return nums;   
+        }
+        return ans;
     }
 };
